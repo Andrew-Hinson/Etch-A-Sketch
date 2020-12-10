284dd3c7097605eff87166b46ff9fceb85b42e76
LivingWill <william.hinson@outlook.com>
2020-12-10 07:30:28 -0600

    created size changer, columns not fixed yet

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 let gridsize = document.querySelector('#gridsize');
 const container = document.querySelector('#container');
+const start = document.querySelector('#start');
+let isGrid = true;
+let mainGrid = 0;
 
-for(i = 0; i < 256; i++){
-    const sketchDiv = document.createElement('div');
-    sketchDiv.setAttribute('class', 'squares');
-    sketchDiv.innerText = 'div';
-    container.appendChild(sketchDiv);
-}
-let hover = document.querySelectorAll('.squares');
+let removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+};
 
-for(i = 0; i < 256; i++){
-    hover[i].addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = "purple";
-    });
+let generateGrid = () => {
+    mainGrid = parseInt(gridsize.value * gridsize.value);
+        for(i = 0; i < mainGrid; i++){
+            const sketchDiv = document.createElement('div');
+            sketchDiv.setAttribute('class', 'squares');
+            sketchDiv.innerText = '(O.O)';
+            container.appendChild(sketchDiv);
+        }
 }
+generateGrid();
+
+let activeHover = () => {
+    mainGrid = parseInt(gridsize.value * gridsize.value);
+    let hover = document.querySelectorAll('.squares');
+    for(i = 0; i < mainGrid; i++){
+        hover[i].addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor = "purple";
+        });
+    }
+}
+while(isGrid){
+    activeHover();
+    isGrid = false;
+}
+
+gridsize.addEventListener('change', () => {
+    removeAllChildNodes(container);
+    generateGrid();
+    isGrid = true;
+    while(isGrid){
+        activeHover();
+        isGrid = false;
+    }
+});
